7b96015da615e3bef18ccd710465fa6ca1d360c7
akshatgoel92 <akshatgoel92@hotmail.com>
2020-10-23 14:51:45 +0530

    Add finite differences function and first pass at plotting function

--- helpers.py
+++ helpers.py
@@ -1,6 +1,5 @@
 # Import packages
-
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import numpy 
 
 
@@ -17,28 +16,63 @@ def get_data():
     pass
 
 
-def get_finite_diff_grad():
+def get_finite_differences(f, x, h):
     '''
-    ----------------------
-    Input: 
-    Output: 
+    -----------------------------------
+    Returns the gradient of function f
+    between point x and point x + h using
+    the finite differences method.
+    -----------------------------------
+    Parameters
+    ----------
+    f: Function to differentiate
+    x: Point at which to differentiate function
+    h: Size of interval over which to calculate gradient
 
-    Description: 
-    -----------------------
+    Outputs: 
+    -----------
+    derivative: float or array of calculated derivatives
+    -----------------------------------
     '''
-    pass
+    derivative = (f(x + h)  - f(x))/h 
+
+    return(derivative)
 
 
-def get_loss_plot():
+def get_loss_plot(train_loss, test_loss):
     '''
-    ----------------------
-    Input: 
-    Output: 
-
-    Description: 
-    -----------------------
+    -----------------------------------
+    Saves plots of epoch vs. loss on
+    the training and validation tests
+    respectively. 
+    -----------------------------------
+    Parameters
+    ----------
+    train_loss: Array of training losses
+    test_loss: Array of testing losses
+    
+    Outputs: 
+    -----------
+    Plot of training and testing loss by epoch
+    -----------
     '''
-    pass
+    
+    # Calculate the no. of epochs training ran for
+    # Assumes that 1 training_loss entry is 1 epoch
+    epoch = range(1, len(training_loss) + 1)
+    
+    # Add lines for each dataset
+    plt.plot(epoch, train_loss, 'r--')
+    plt.plot(epoch, test_loss, 'b-')
+    
+    # Add annotations
+    plt.legend(['Training Loss', 'Test Loss'])
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    
+    # Show and save
+    plt.show()
+    plt.savefig(output_path)
 
 
 def main():
