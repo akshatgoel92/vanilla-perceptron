4192859c5e95e0aa0786f24f56a2045443f5a952
akshatgoel92 <akshatgoel92@hotmail.com>
2020-10-23 13:42:51 +0530

    Add first pass at helpers file

---
+++ helpers.py
@@ -0,0 +1,57 @@
+# Import packages
+
+from matplotlib import pyplot as plt
+import numpy 
+
+
+
+def get_data(): 
+    '''
+    ----------------------
+    Input: 
+    Output: 
+
+    Description: 
+    -----------------------
+    '''
+    pass
+
+
+def get_finite_diff_grad():
+    '''
+    ----------------------
+    Input: 
+    Output: 
+
+    Description: 
+    -----------------------
+    '''
+    pass
+
+
+def get_loss_plot():
+    '''
+    ----------------------
+    Input: 
+    Output: 
+
+    Description: 
+    -----------------------
+    '''
+    pass
+
+
+def main():
+    '''
+    ----------------------
+    Input: 
+    Output: 
+
+    Description: 
+    -----------------------
+    '''
+    pass
+
+
+if __name__ = '__main__':
+    pass
