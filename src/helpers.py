4c4e50f727ddfb32f75cbe6a02339b40cb39ada6
akshatgoel92 <akshatgoel92@hotmail.com>
2020-11-01 01:11:39 +0530

    Move utility functions from 03 to helpers - changes have been tested and they reproduce exactly the same results as before

--- src/helpers.py
+++ src/helpers.py
@@ -383,6 +383,34 @@ def get_results(X_dev, y_dev, history, best_epoch, label="dev"):
     print(f"{label} set accuracy: {accuracy}")
     
     return(accuracy)
+
+
+def get_best_results(history):
+    '''
+    --------------------
+    Prepare data
+    --------------------
+    Parameters: 
+    weights: Current set of weights
+    biases: Current set of biases
+    gradients: Current set of gradients
+    learning_rate: parameter to guide SGD step size
+    --------------------
+    Output: 
+    Updated weights and biases
+    --------------------
+    '''
+    # Store results
+    best_epoch = np.array(history["losses"]).argmin()
+    best_accuracy = history['accuracies'][best_epoch]
+    best_loss = history['losses'][best_epoch]
+    
+    # Display results
+    print(f"best accuracy: {history['accuracies'][best_epoch]}")
+    print(f"best loss: {history['losses'][best_epoch]}")
+    print(f"best epoch: {best_epoch}")
+    
+    return(best_epoch, best_accuracy, best_loss)
     
 
 if __name__ == '__main__':
